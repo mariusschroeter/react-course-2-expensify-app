164666aaebb9c84eb4baf7a436178c61aa749deb
Marius Schröter <marius94@web.de>
2019-02-26 08:31:31 +0100

    Remove expense data

--- src/App.js
+++ src/App.js
@@ -3,15 +3,7 @@ import AppRouter from "./routers/AppRouter"
 import './styles/App.css';
 import { Provider } from "react-redux"
 import configureStore from "./store/configureStore"
-import { addExpense } from "./actions/expenses"
-// import { setTextFilter } from "./actions/filters"
-
-
 const store = configureStore()
-
-store.dispatch(addExpense({ description: "Water bill", amount: 30, createdAt: 100 }))
-store.dispatch(addExpense({ description: "Stone bill", amount: 10, createdAt: 300 }))
-store.dispatch(addExpense({ description: "Heating bill", amount: 20, createdAt: 200 }))
 
 class App extends Component {
   render() {
