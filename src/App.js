26848942132fef51b6d511f7381b4109ae3b13f4
Marius Schröter <marius94@web.de>
2019-02-26 09:09:47 +0100

    Test commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import AppRouter from "./routers/AppRouter"
 import './styles/App.css';
 import { Provider } from "react-redux"
 import configureStore from "./store/configureStore"
+
 const store = configureStore()
+
+store.dispatch(addExpense({ description: "Water bill", amount: 100, createdAt: 500 }))
 
 class App extends Component {
   render() {
